197bb95da8283323a08b8507ec0b23aa8d0c881c
hperry711 <hunterhperry@gmail.com>
2013-06-05 11:28:42 -0400

    .

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -1,21 +1,53 @@
 # Flower excercise (4.2) from Week 0
 
-# Name:
+# Name: Hunter Perry
 
 
 from TurtleWorld import * 		
-world = TurtleWorld()			
-bob = Turtle()				
 
+import math
+bob.delay = 0.00001
+world.clear()
+bob = Turtle()
+def arc (turtle, r, arc_angle):
 
+    n_sides = arc_angle				
+    segment_length = 2*math.pi*r/360
+    turn_angle = 1
+    for i in range (n_sides):
+	fd (turtle, segment_length)
+	lt (turtle, turn_angle)
 
-# This is where you put code to move bob
+def petal (turtle, r, arc_angle):
 
+    turn_angle = 180 - arc_angle
 
+    arc (turtle,r, arc_angle)
+    lt (bob, turn_angle)
+    arc (turtle, r, arc_angle)
+    lt (bob, turn_angle)
 
+def flower (turtle, r, n, angle):
 
+    for i in range (n):
+	arc_angle = angle
+	turn_angle = 360/n  	
 
+	petal (turtle, r, arc_angle)
+	lt (turtle, turn_angle)
 
+def move_turtle (turtle, n):
+
+    pu (bob)
+    fd (bob, n)
+    pd (bob)
+
+radius = 400
+arc_theta = 30
+n_petals = 10
+
+move_turtle (bob, 200)
+flower (bob, radius, n_petals, arc_theta)
 
 
 wait_for_user()					
--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,20 +1,33 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Hunter Perry
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
+def square(turtle, n):
+    for i in range(4):
+    fd(turtle, n)
+    lt(turtle)
+square(bob, 90)
 
+def polygon(turtle, length, n):
+    for i in range(n):
+    fd(turtle, length)
+    lt(turtle, (360.0/n))
+polygon(bob, 5, 100)
 
+def circle(turtle, radius):
+    polygon(turtle, radius, 9)
+circle(bob, 40)
 
-# This is where you put code to move bob
+def arc(turtle, radius, theta):
+    for i in range(theta):
+    fd(turtle, ((math.pi * radius * 2) / 360))
+    lt(turtle, 1)
 
-
-
-
-
+arc(bob, 8, 90)
 
 
 
